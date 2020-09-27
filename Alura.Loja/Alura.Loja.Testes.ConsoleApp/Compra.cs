670bd59998e13ed87e2331bc489c60b972b11d68
gilvan <g.r.almeida@live.com>
2020-09-26 22:36:14 -0300

    Relacionamento um para muitos

---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Compra.cs
@@ -0,0 +1,9 @@
+﻿namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Compra
+    {
+        public int Quantidade { get; internal set; }
+        public Produto Produto { get; internal set; }
+        public double Preco { get; internal set; }
+    }
+}
--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,18 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Alura.Loja.Testes.ConsoleApp
+﻿namespace Alura.Loja.Testes.ConsoleApp
 {
     partial class Program
     {
         static void Main(string[] args)
         {
-               
+            var paoFrances = new Produto
+            {
+                Nome = "Pão Francês",
+                PrecoUnitario = 0.40,
+                Unidade = "Unidade",
+                Categoria = "Padaria"
+            };
+
+            var compra = new Compra();
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
         }
 
     }
