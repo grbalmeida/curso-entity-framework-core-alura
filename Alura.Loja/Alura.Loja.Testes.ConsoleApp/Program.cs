853d89cd7b6638f9634a7318c5b2bcad290196a8
gilvan <g.r.almeida@live.com>
2020-09-26 22:59:25 -0300

    Inserindo objetos relacionados

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace Alura.Loja.Testes.ConsoleApp
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace Alura.Loja.Testes.ConsoleApp
 {
     partial class Program
     {
@@ -16,6 +23,19 @@
             compra.Quantidade = 6;
             compra.Produto = paoFrances;
             compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            using (var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                contexto.Compras.Add(compra);
+
+                contexto.SaveChanges();
+            }
+
+            Console.ReadLine();
         }
 
     }
